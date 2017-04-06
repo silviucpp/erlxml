9456ad099bfadd805675b241feda9cd3dfa15346
Silviu Caragea <silviu.caragea@woowteam.com>
2017-04-06 12:22:06 +0300

    Improve iterations over dom

--- c_src/erlxml.cc
+++ c_src/erlxml.cc
@@ -3,6 +3,9 @@
 #include "nif_utils.h"
 #include "xmlstreamparser.h"
 
+//instead reversing the list in erlang we traverse the dom in reverse order
+#define REVERSE_ITERATION
+
 static const char kErrorFailedToAllocXmlStream[] = "failed to alloc stream object";
 static const char kErrorBadOwner[] = "erlxml session was created on a different process";
 
@@ -15,7 +18,7 @@ struct enif_erlxml_stream
 struct stream_options
 {
     stream_options() : skip_root(true), stanza_limit(0) {}
-    
+
     bool skip_root;
     size_t stanza_limit;
 };
@@ -31,9 +34,9 @@ struct parser_data
 void enif_stream_parser_free(ErlNifEnv* env, void* obj)
 {
     UNUSED(env);
-    
+
     enif_erlxml_stream* stream = static_cast<enif_erlxml_stream*>(obj);
-    
+
     if(stream->parser != NULL)
         delete stream->parser;
 }
@@ -47,32 +50,51 @@ void node_walker(ErlNifEnv*env, const pugi::xml_node& node, ERL_NIF_TERM* list)
             ERL_NIF_TERM name = make_binary(env, node.name(), strlen(node.name()));
             ERL_NIF_TERM attrs = enif_make_list(env, 0);
             ERL_NIF_TERM childrens = enif_make_list(env, 0);
-            
+
+#if defined(REVERSE_ITERATION)
+            for (pugi::xml_attribute_iterator ait = node.attributes_end(); ait != node.attributes_begin();)
+            {
+                --ait;
+                ERL_NIF_TERM key = make_binary(env, ait->name(), strlen(ait->name()));
+                ERL_NIF_TERM value = make_binary(env, ait->value(), strlen(ait->value()));
+                attrs = enif_make_list_cell(env, enif_make_tuple2(env, key, value), attrs);
+            }
+#else
             for (pugi::xml_attribute attr : node.attributes())
             {
                 ERL_NIF_TERM key = make_binary(env, attr.name(), strlen(attr.name()));
                 ERL_NIF_TERM value = make_binary(env, attr.value(), strlen(attr.value()));
                 attrs = enif_make_list_cell(env, enif_make_tuple2(env, key, value), attrs);
             }
-            
+
+            enif_make_reverse_list(env, attrs, &attrs);
+#endif
+
+#if defined(REVERSE_ITERATION)
+            for (pugi::xml_node_iterator nit = node.end(); nit != node.begin();)
+            {
+                --nit;
+                node_walker(env, *nit, &childrens);
+            }
+#else
             for (pugi::xml_node child: node.children())
                 node_walker(env, child, &childrens);
-            
-            ERL_NIF_TERM childrens_reverse;
-            enif_make_reverse_list(env, childrens, &childrens_reverse);
-            
-            ERL_NIF_TERM xmlel = enif_make_tuple4(env, ATOMS.atomXmlel, name, attrs, childrens_reverse);
+
+            enif_make_reverse_list(env, childrens, &childrens);
+#endif
+
+            ERL_NIF_TERM xmlel = enif_make_tuple4(env, ATOMS.atomXmlel, name, attrs, childrens);
             *list = enif_make_list_cell(env, xmlel, *list);
             break;
         }
-        
+
         case pugi::node_pcdata:
         {
             ERL_NIF_TERM value = make_binary(env, node.value(), strlen(node.value()));
             *list = enif_make_list_cell(env, enif_make_tuple2(env, ATOMS.atomXmlcdata, value), *list);
             break;
         }
-            
+
         default:;
     }
 }
@@ -87,19 +109,19 @@ ERL_NIF_TERM parse_stream_options(ErlNifEnv* env, ERL_NIF_TERM list, stream_opti
 {
     if(!enif_is_list(env, list))
         return make_bad_options(env, list);
-    
+
     ERL_NIF_TERM head;
     const ERL_NIF_TERM *items;
     int arity;
-    
+
     while(enif_get_list_cell(env, list, &head, &list))
     {
         if(!enif_get_tuple(env, head, &arity, &items) || arity != 2)
             return make_bad_options(env, head);
-        
+
         ERL_NIF_TERM key = items[0];
         ERL_NIF_TERM value = items[1];
-        
+
         if(enif_is_identical(key, ATOMS.atomStanzaLimit))
         {
             if(!get_boolean(value, &opts->skip_root))
@@ -115,34 +137,34 @@ ERL_NIF_TERM parse_stream_options(ErlNifEnv* env, ERL_NIF_TERM list, stream_opti
             return make_bad_options(env, head);
         }
     }
-    
+
     return ATOMS.atomOk;
 }
 
 ERL_NIF_TERM enif_stream_parser_new(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     UNUSED(argc);
-    
+
     erlxml_data* data = static_cast<erlxml_data*>(enif_priv_data(env));
-    
+
     stream_options opts;
-    
+
     ERL_NIF_TERM parse_result = parse_stream_options(env, argv[0], &opts);
-    
+
     if(!enif_is_identical(parse_result, ATOMS.atomOk))
         return parse_result;
-    
+
     enif_erlxml_stream* nif_stream = static_cast<enif_erlxml_stream*>(enif_alloc_resource(data->res_xml_stream_parser, sizeof(enif_erlxml_stream)));
-    
+
     if(nif_stream == NULL)
         return make_error(env, kErrorFailedToAllocXmlStream);
-    
+
     ErlNifPid current_pid;
     enif_self(env, &current_pid);
 
     nif_stream->parser = new XmlStreamParser(opts.skip_root, opts.stanza_limit, handle_stanza);
     nif_stream->owner_pid = enif_make_pid(env, &current_pid);
-    
+
     ERL_NIF_TERM term = enif_make_resource(env, nif_stream);
     enif_release_resource(nif_stream);
     return enif_make_tuple2(env, ATOMS.atomOk, term);
@@ -151,39 +173,39 @@ ERL_NIF_TERM enif_stream_parser_new(ErlNifEnv* env, int argc, const ERL_NIF_TERM
 ERL_NIF_TERM enif_stream_parser_feed(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     UNUSED(argc);
-    
+
     erlxml_data* data = static_cast<erlxml_data*>(enif_priv_data(env));
-    
+
     enif_erlxml_stream* stream = NULL;
     ErlNifBinary bin;
-    
+
     if(!enif_get_resource(env, argv[0], data->res_xml_stream_parser, (void**) &stream))
         return make_badarg(env);
-    
+
     if(!get_binary(env, argv[1], &bin))
         return make_badarg(env);
-    
+
     ErlNifPid current_pid;
-    
+
     if(enif_self(env, &current_pid) && !enif_is_identical(stream->owner_pid, enif_make_pid(env, &current_pid)))
         return make_error(env, kErrorBadOwner);
-    
+
     parser_data parser_data(env, enif_make_list(env, 0));
     XmlStreamParser::parse_result result = stream->parser->FeedData(bin.data, bin.size, &parser_data);
-    
+
     consume_timeslice(env, bin.size);
-    
+
     switch (result)
     {
         case XmlStreamParser::kParseOk:
                 return make_ok_result(env, parser_data.term);
-        
+
         case XmlStreamParser::kParseInvalidXml:
             return make_error(env, ATOMS.atomErrorInvalidStanza);
-        
+
         case XmlStreamParser::kParseStanzaLimitHit:
             return make_error(env, ATOMS.atomErrorMaxStanzaLimitHit);
-            
+
         default:
             return make_error(env, "unknown error");
     }
@@ -192,23 +214,23 @@ ERL_NIF_TERM enif_stream_parser_feed(ErlNifEnv* env, int argc, const ERL_NIF_TER
 ERL_NIF_TERM enif_stream_parser_reset(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     UNUSED(argc);
-    
+
     erlxml_data* data = static_cast<erlxml_data*>(enif_priv_data(env));
-    
+
     enif_erlxml_stream* stream = NULL;
     ErlNifPid current_pid;
-    
+
     if(!enif_get_resource(env, argv[0], data->res_xml_stream_parser, (void**) &stream))
         return make_badarg(env);
-    
+
     if(enif_self(env, &current_pid) && !enif_is_identical(stream->owner_pid, enif_make_pid(env, &current_pid)))
         return make_error(env, kErrorBadOwner);
-    
+
     bool skip_root;
-    
+
     if(!get_boolean(argv[1], &skip_root))
         return make_badarg(env);
-    
+
     stream->parser->Reset(skip_root);
     return ATOMS.atomOk;
 }
@@ -216,26 +238,26 @@ ERL_NIF_TERM enif_stream_parser_reset(ErlNifEnv* env, int argc, const ERL_NIF_TE
 ERL_NIF_TERM enif_dom_parse(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     UNUSED(argc);
-    
+
     ErlNifBinary bin;
-    
+
     if(!get_binary(env, argv[0], &bin))
         return make_badarg(env);
-    
+
     pugi::xml_document pugi_doc;
-    
-    if(pugi_doc.load_buffer_inplace(bin.data, bin.size).status != pugi::status_ok)
+
+    if(pugi_doc.load_buffer_inplace(bin.data, bin.size, pugi::parse_default).status != pugi::status_ok)
         return make_error(env, ATOMS.atomErrorInvalidStanza);
-    
+
     ERL_NIF_TERM list = enif_make_list(env, 0);
     node_walker(env, pugi_doc.first_child(), &list);
-    
+
     ERL_NIF_TERM head;
     ERL_NIF_TERM tail;
-    
+
     if(!enif_get_list_cell(env, list, &head, &tail))
         return make_error(env, ATOMS.atomErrorInvalidStanza);
-    
+
     return make_ok_result(env, head);
 }
 
