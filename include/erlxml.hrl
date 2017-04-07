2ccf1ced85e1ca96ea477fae58b1c1816012e0a4
Silviu Caragea <silviu.caragea@woowteam.com>
2017-04-07 16:59:08 +0300

    Implement typespecs

--- include/erlxml.hrl
+++ include/erlxml.hrl
@@ -7,3 +7,5 @@
 -record(xmlel, {name :: binary(), attrs = [] :: [xmlattr()], children =  [] :: [#xmlel{} | #xmlcdata{}]}).
 
 -type xmlterm() :: #xmlel{} | xmlattr() | #xmlcdata{}.
+-type erlxml_option():: {skip_root, boolean()} | {stanza_limit, non_neg_integer()}.
+-type reason() :: invalid_stanza | max_stanza_limit_hit | badarg | binary().
--- src/erlxml.erl
+++ src/erlxml.erl
@@ -4,34 +4,46 @@
 -include("erlxml.hrl").
 
 -export([
-
-    %stream based parsing
-
     new_stream/0,
     new_stream/1,
     parse_stream/2,
-    parse_reset/2,
-
-    %dom based parsing
-
+    reset_stream/2,
     parse/1,
     to_binary/1
 ]).
 
+-spec new_stream() ->
+    {ok, reference()} | {error, reason()}.
+
 new_stream() ->
     new_stream([]).
+
+-spec new_stream([erlxml_option()]) ->
+    {ok, reference()} | {error, reason()}.
 
 new_stream(Options) ->
     erlxml_nif:new_stream(Options).
 
+-spec parse_stream(reference(), iolist() | binary()) ->
+    {ok, [#xmlel{}]} | {error, reason()}.
+
 parse_stream(Parser, Data) ->
     erlxml_nif:chunk_feed_stream(Parser, Data).
 
-parse_reset(Parser, SkipRoot) ->
+-spec reset_stream(reference(), boolean()) ->
+    ok | {error, reason()}.
+
+reset_stream(Parser, SkipRoot) ->
     erlxml_nif:reset_stream(Parser, SkipRoot).
+
+-spec parse(iolist() | binary()) ->
+    {ok, [#xmlel{}]} | {error, reason()}.
 
 parse(Data) ->
     erlxml_nif:dom_parse(Data).
+
+-spec to_binary(#xmlel{}) ->
+    binary() | {error, reason()}.
 
 to_binary(Data) ->
     erlxml_nif:to_binary(Data).
