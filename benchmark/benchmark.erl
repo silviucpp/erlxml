fbca3816eb257caea2993e28580c70b1be6a46ec
Silviu Caragea <silviu.caragea@woowteam.com>
2017-04-07 15:05:29 +0300

    Add benchmark scripts

---
+++ benchmark/benchmark.erl
@@ -0,0 +1,105 @@
+-module(benchmark).
+-author("silviu.caragea").
+
+-export([
+    bench_encoding/3,
+    bench_parsing/3
+]).
+
+-define(ELEMENT, {xmlel,<<"iq">>,
+    [{<<"type">>,<<"get">>}],
+    [{xmlel,<<"query">>,
+        [{<<"xmlns">>,<<"jabber:iq:bulk">>}],
+        [{xmlel,<<"r">>, [{<<"ver">>,<<"1489702723756">>},{<<"client">>,<<"0.41.16">>}], []},
+            {xmlel,<<"b">>,[{<<"ver">>,<<"1470998323471">>}],[]},
+            {xmlel,<<"pf">>, [{<<"type">>,<<"roster">>},{<<"ver">>,<<"1473925451360">>}], []},
+            {xmlel,<<"pf">>, [{<<"type">>,<<"addressbook">>},{<<"ver">>,<<"1410177959174">>}], []}]},
+        {xmlel,<<"message">>,
+            [{<<"from">>,<<"user@wdomain/resource-TMnXEhgkGN">>},
+                {<<"to">>,<<"user2@domain/mac70c36269">>},
+                {<<"ts">>,<<"1490255206161">>},
+                {<<"id">>,<<"YgP1z-18834681">>},
+                {<<"type">>,<<"headline">>}],
+            [{xmlel,<<"backendmessage">>,
+                [{<<"xmlns">>,<<"notification">>},{<<"push">>,<<"0">>}],
+                [{xmlel,<<"resreceived">>,[],
+                    [{xmlel,<<"accountId">>,[],
+                        [{xmlcdata,<<"23423534534">>}]},
+                        {xmlel,<<"amount">>,[],
+                            [{xmlcdata,
+                                <<"0.200000000000000">>}]},
+                        {xmlel,<<"type">>,[],
+                            [{xmlcdata,<<"AEW-12">>}]},
+                        {xmlel,<<"description">>,[],
+                            [{xmlcdata,<<"884340">>}]}]}]}]}]}
+).
+
+-define(STANZA, <<"<iq type='get'>
+   <query xmlns='jabber:iq:bulk'>
+     <r ver='1489702723756' client='0.41.16'/>
+     <b ver='1470998323471'/>
+     <pf type='roster' ver='1473925451360'/>
+     <pf type='addressbook' ver='1410177959174'/>
+   </query>
+   <message from='user@wdomain/resource-TMnXEhgkGN' to='user2@domain/mac70c36269' ts='1490255206161' id='YgP1z-18834681' type='headline'>
+   <backendmessage xmlns='notification' push='0'>
+     <resreceived>
+       <accountId>
+         23423534534
+       </accountId>
+       <amount>
+         0.200000000000000
+       </amount>
+       <type>
+         AEW-12
+       </type>
+       <description>
+         884340
+       </description>
+     </resreceived>
+   </backendmessage>
+    </message>
+ </iq>">>).
+
+bench_encoding(Engine, Number, Concurrency) ->
+    Fun = fun() -> to_binary(Engine) end,
+    bench(Fun, Number, Concurrency).
+
+bench_parsing(Engine, Number, Concurrency) ->
+    Fun = fun() -> parse(Engine, ?STANZA) end,
+    bench(Fun, Number, Concurrency).
+
+bench(Fun, Number, Concurrency) ->
+    Self = self(),
+    List = lists:seq(1, Concurrency),
+    LoopNumbers = Number div Concurrency,
+
+    A = os:timestamp(),
+    Pids = [spawn_link(fun() -> loop(LoopNumbers, Fun), Self ! {self(), done} end) || _ <- List],
+    [receive {Pid, done} -> ok end || Pid <- Pids],
+    B = os:timestamp(),
+
+    print(Number, A, B).
+
+print(Num, A, B) ->
+    Microsecs = timer:now_diff(B, A),
+    Milliseconds = Microsecs/1000,
+    Secs = Milliseconds/1000,
+    StanzaPerSec = Num/Secs,
+    io:format("### ~p ms ~.2f stanza/sec ~n", [Milliseconds, StanzaPerSec]).
+
+loop(0, _Fun) ->
+    ok;
+loop(Nr, Fun) ->
+    Fun(),
+    loop(Nr-1, Fun).
+
+to_binary(erlxml) ->
+    erlxml:to_binary(?ELEMENT);
+to_binary(exml) ->
+    exml:to_binary(?ELEMENT).
+
+parse(erlxml, Data) ->
+    {ok, _} = erlxml:parse(Data);
+parse(exml, Data) ->
+    {ok, _} = exml:parse(Data).
---
+++ benchmark/benchmark_stream.erl
@@ -0,0 +1,104 @@
+-module(benchmark_stream).
+-author("silviu.caragea").
+
+-define(CHUNK_SIZE, 1024).
+
+-export([bench/4]).
+
+bench(Module, File, Number, Concurrency) ->
+    {Chunks, BinarySize} = readlines(File, ?CHUNK_SIZE),
+
+    Self = self(),
+    List = lists:seq(1, Concurrency),
+    LoopNumbers = Number div Concurrency,
+
+    ProcFun = fun() ->
+        {ok, Parser} = new_parser(Module),
+        NewParser1 = run_parser([<<"<stream>">>], Module, Parser),
+        NewParser2 = loop(LoopNumbers, Chunks, Module, NewParser1),
+        run_parser([<<"</stream>">>], Module, NewParser2),
+        Self ! {self(), done}
+    end,
+
+    A = os:timestamp(),
+    Pids = [spawn_link(ProcFun) || _ <- List],
+    [receive {Pid, done} -> ok end || Pid <- Pids],
+    B = os:timestamp(),
+
+    print(BinarySize*Number, A, B).
+
+loop(0, _Chunks, _Module, Parser) ->
+    Parser;
+loop(Nr, Chunks, Module, Parser) ->
+    NewParser = run_parser(Chunks, Module, Parser),
+    loop(Nr-1, Chunks, Module, NewParser).
+
+run_parser([H|T], Module, Parser) ->
+    case stream_parse(Module, Parser, H) of
+        {ok, _} ->
+            run_parser(T, Module, Parser);
+        {ok, NewParser, _} ->
+            run_parser(T, Module, NewParser)
+    end;
+run_parser([], _Module, Parser) ->
+    Parser.
+
+new_parser(erlxml) ->
+    erlxml:new_stream([{stanza_limit, 65000}]);
+new_parser(exml) ->
+    exml_stream:new_parser();
+new_parser(dummy) ->
+    {ok, null}.
+
+stream_parse(erlxml, Parser, Data) ->
+    erlxml:parse_stream(Parser, Data);
+stream_parse(exml, Parser , Data) ->
+    exml_stream:parse(Parser, Data);
+stream_parse(dummy, _Parser , _Data) ->
+    {ok, []}.
+
+readlines(FileName, LengthChunks) ->
+    {ok, Device} = file:open(FileName, [read]),
+    Lines = get_lines(Device),
+    Binary = binary_join(Lines),
+    Size = byte_size(Binary),
+    {build_chunks(Binary, LengthChunks, []), Size}.
+
+build_chunks(Binary, Length, Acc) ->
+    case byte_size(Binary) > Length of
+        true ->
+            <<Chunk:Length/binary, Rest/binary>>  = Binary,
+            build_chunks(Rest, Length, [Chunk | Acc]);
+        _ ->
+            lists:reverse([Binary|Acc])
+    end.
+
+get_lines(Device) ->
+    lists:reverse(get_lines(Device, [])).
+
+get_lines(Device, Accum) ->
+    case io:get_line(Device, "") of
+        eof  ->
+            file:close(Device), Accum;
+        Line ->
+            get_lines(Device, [list_to_binary(Line)|Accum])
+    end.
+
+binary_join([Part]) ->
+    Part;
+binary_join([Head|Tail]) ->
+    lists:foldl(fun (Value, Acc) -> <<Acc/binary , Value/binary>> end, Head, Tail).
+
+print(Bytes, A, B) ->
+    Microsecs = timer:now_diff(B, A),
+    Milliseconds = Microsecs/1000,
+    Secs = Milliseconds/1000,
+    BytesPerSec = Bytes/Secs,
+    io:format("### ~p ms ~s bytes/sec total bytes processed: ~s ~n", [Milliseconds, format_size(BytesPerSec), format_size(Bytes)]).
+
+format_size(Size) ->
+    format_size(Size, ["B","KB","MB","GB","TB","PB"]).
+
+format_size(S, [_|[_|_] = L]) when S >= 1024 -> format_size(S/1024, L);
+format_size(S, [M|_]) ->
+    io_lib:format("~.2f ~s", [float(S), M]).
